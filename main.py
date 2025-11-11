dea441dfd9475188cc6dd558e9836e38123f7eb9
Durnathi <zalinmaster@yahoo.com>
2025-11-11 15:56:25 -0500

    Finnished bookbot sorting lesson

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from stats import get_book_text, get_num_words, get_letter_count
+from stats import get_num_words, get_letter_count, dict_sort
 
 def main ():
-    with open("books/frankenstein.txt") as file:
-        get_num_words(file)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    letter_count = get_letter_count(text)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for k, v in sorted(letter_count.items(), key=dict_sort, reverse=True):
+        print(f"{k}: {v}")
+    print("============= END ===============")
+    
+def get_book_text(path):
+    with open(path) as book:
+        return book.read()
 
 main()
--- stats.py
+++ stats.py
@@ -1,21 +1,17 @@
-def get_book_text(book):
-    text = (f"{book.read()}")
-    return text.split()
-
 def get_num_words(book):
-    num_words = len(get_book_text(book))
-    print(f"Found {num_words} total words")
+    words = book.split()
+    return len(words)
     
 def get_letter_count(book):
-    letters = []
-    letters_count = []
-    words = get_book_text(book)
-    for word in words:
-        mixed_letters = word.split()
-        letters.append(mixed_letters.lower())
-    for letter in letters:
-        if letter == "a"
-            if letters_count != 0
-                letters_count[0] += 1
-            else letters_count[0] = 1
-        elif
+    letters = {}
+    for letter in book:
+        lowered = letter.lower()
+        if lowered.isalpha():
+            if lowered in letters:
+                letters[lowered] += 1
+            else:
+                letters[lowered] = 1
+    return letters
+    
+def dict_sort(key):
+    return key[1]
